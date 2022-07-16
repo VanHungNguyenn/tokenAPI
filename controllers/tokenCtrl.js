bfb33522fd23a49ebbe6193f5e3f17eb4ceaa016
VanHungNguyen <vanhungnguyenbku@gmail.com>
2022-07-16 18:01:59 +0700

    create token

--- controllers/tokenCtrl.js
+++ controllers/tokenCtrl.js
@@ -1,5 +1,21 @@
 const Token = require('../models/tokenModel')
 
-const tokenCtrl = {}
+const tokenCtrl = {
+	create: async (req, res) => {
+		try {
+			const { uid, token } = req.body
+
+			if (!uid || !token) {
+				return res.status(400).json({ message: 'No uid or token' })
+			}
+
+			const newToken = new Token({ uid, token })
+			await newToken.save()
+			res.status(200).json({ message: 'Token created' })
+		} catch (error) {
+			return res.status(500).json({ message: error.message })
+		}
+	},
+}
 
 module.exports = tokenCtrl
--- routes/tokenRouter.js
+++ routes/tokenRouter.js
@@ -1,4 +1,6 @@
 const router = require('express').Router()
 const tokenCtrl = require('../controllers/tokenCtrl')
 
+router.post('/create', tokenCtrl.create)
+
 module.exports = router
